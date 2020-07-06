5fe47bed03f7464374075282d29249f57dde3a95
MrNiceRicee <47019696+MrNiceRicee@users.noreply.github.com>
2020-07-05 20:08:35 -0700

    Initial commit

---
+++ LoginApp/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using LoginApp.Models;
+using LoginApp.Services.Business;
+using LoginApp.Services.Business.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public ActionResult Login(User user)
+        {
+            SecurityServices securityServices = new SecurityServices();
+            if (securityServices.Authenticate(user))
+            {
+                return View("LoginSuccess",user);
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+
+        public ActionResult Register(User user)
+        {
+            SecurityDAO securityDAO = new SecurityDAO();
+            securityDAO.RegisterUser(user);
+            return View("Register");
+        }
+    }
+}
---
+++ LoginApp/Services/Business/SecurityServices.cs
@@ -0,0 +1,19 @@
+﻿using LoginApp.Models;
+using LoginApp.Services.Business.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginApp.Services.Business
+{
+    public class SecurityServices
+    {
+
+        public bool Authenticate(User user)
+        {
+            SecurityDAO service = new SecurityDAO();
+            return service.CheckUserCreds(user);
+        }
+    }
+}
